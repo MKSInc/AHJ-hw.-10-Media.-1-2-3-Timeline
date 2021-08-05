dec4ed7f788215a708ff1cb6b0c3b1fc314e0996
Maksim <pyshkov.maksim@gmail.com>
2021-08-05 11:37:38 +0300

    feat: html, css added

--- webpack.common.js
+++ webpack.common.js
@@ -31,6 +31,14 @@ module.exports = (buildMode) => ({
           MiniCssExtractPlugin.loader, 'css-loader',
         ],
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
